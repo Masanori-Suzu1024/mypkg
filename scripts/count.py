af7eb39df267b0757aa09c06485f69a19f737cd9
Masanori-Suzu1024 <92065217+Masanori-Suzu1024@users.noreply.github.com>
2022-01-03 18:45:34 +0900

    Update count.py
    なぜかbranchがmainからmasterに切り替わっていたためマージした

--- scripts/count.py
+++ scripts/count.py
@@ -3,10 +3,41 @@ import rospy
 from std_msgs.msg import Int32
 
 rospy.init_node('count')
-pub = rospy.Publisher('count_up', Int32, queue_size=1)
-rate = rospy.Rate(10)
+pub = rospy.Publisher('count_up', Int32, queue_size=10)
+rate = rospy.Rate(1)
 n = 0
+val = input('Enter character`s strength: ')
+c_strength = int(val)
+print(val)
+val = input('Enter arms strength: ')
+a_strength = int(val)
+print(val)
+val = input('Enter Tenp magnification(%):')
+tenp = float(val)
+print(val)
+val = input('Enter damage buf(%):')
+damage_buf = float(val)
+print(val)
+base_damage = (c_strength + a_strength) * tenp / 100
+print(base_damage)
+val = input('Enter Critical rate(%):')
+critical_rate = float(val)
+print(val)
+val = input('Enter Critical damage magnification(%):')
+critical_damage = float(val)
+print(val)
+critical_correct = 1 + ((critical_rate / 100) * (critical_damage / 100))
+print(critical_correct)
+damage = base_damage * critical_correct * (damage_buf / 100)
+print('base_damage:')
+print(base_damage)
+print('critical correction value(ideal value):')
+print(critical_correct)
+print('total_damage:')
+print(damage)
+n = int(damage)
+
 while not rospy.is_shutdown():
-    n += 1
+
     pub.publish(n)
     rate.sleep()
